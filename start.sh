a604476c272ee64cd8d027eda2063fe238917b28
zaenalarifin12 <zaenalblaze@gmail.com>
2023-10-18 22:33:38 +0700

    migration via go

--- start.sh
+++ start.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-echo "run db migration"
-source /app/app.env
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+#echo "run db migration"
+#source /app/app.env
+#/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+#chmod +x /app/wait-for.sh
 
 echo "start the app"
 exec "$@"
